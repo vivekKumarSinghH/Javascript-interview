ab6041dd48dfb9a3ccb15244f1fd0c83a5e929b2
Vivek Kumar <72158561+vivekKumarSinghH@users.noreply.github.com>
2025-04-15 01:07:34 +0530

    ➕ add more array pollyfills

--- Polyfills/index.js
+++ Polyfills/index.js
@@ -1,5 +1,7 @@
 let arr = [1, 2, 3, 4, 5];
 
+//------Custom Map----------
+
 Array.prototype.myMap = function (callback, thisArg) {
   let result = [];
   console.log(callback, thisArg);
@@ -20,6 +22,9 @@ Array.prototype.myMap = function (callback, thisArg) {
 let mapResult = arr.myMap((ele, i) => ele * i);
 
 console.log("My map polyfill result ==>", mapResult);
+
+//------ Custom filter ------
+
 Array.prototype.myFilter = function (callback) {
   let result = [];
   console.log(callback, this);
@@ -32,3 +37,41 @@ Array.prototype.myFilter = function (callback) {
 };
 let filterResult = arr.myFilter((ele, i) => i % 2 == 0);
 console.log("My filter polyfill result ==>", filterResult);
+
+//-------Custom reducer ---------
+Array.prototype.customReducer = function (callback, initialValue) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    if (accumulator == undefined) accumulator = this[i];
+    accumulator = callback(accumulator, this[i], i);
+  }
+  return accumulator;
+};
+const sumReducer = arr.customReducer((acc, num) => acc * num);
+console.log("My Reducer polyfill result ==>", sumReducer);
+
+//--------- Custom Find -----------
+
+Array.prototype.customFind = function (callback) {
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i)) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+const findResult = arr.customFind((num) => num > 3);
+console.log("My find polyfill result ==>", findResult);
+
+//------------ Custom Foreach ---------
+Array.prototype.customForEach = function (callback, thisArg) {
+  let result = [];
+  console.log(callback, thisArg);
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i], i, this);
+  }
+};
+
+arr.customForEach((ele, i) =>
+  console.log("My Foreach polyfill result ==>", ele)
+);
